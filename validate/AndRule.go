36f6d80ae3d4f3ba30829b2f7738746570fece98
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-13 23:44:08 -0700

    Ported AndRule and AtLeastOneExistsRule

--- validate/AndRule.go
+++ validate/AndRule.go
@@ -1,3 +1,28 @@
 package validate
 
-// Todo: Complete implementation
+type AndRule struct {
+	rules []IValidationRule
+}
+
+func NewAndRule(rules ...IValidationRule) *AndRule {
+	return &AndRule{
+		rules: rules,
+	}
+}
+
+func (c *AndRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	if c.rules == nil || len(c.rules) == 0 {
+		return nil
+	}
+
+	results := []*ValidationResult{}
+
+	for _, rule := range c.rules {
+		ruleResults := rule.Validate(path, schema, value)
+		if ruleResults != nil {
+			results = append(results, ruleResults...)
+		}
+	}
+
+	return results
+}
--- validate/AtLeastOneExistsRule.go
+++ validate/AtLeastOneExistsRule.go
@@ -1,3 +1,47 @@
 package validate
 
-// Todo: Complete implementation
+import (
+	"strings"
+
+	"github.com/pip-services-go/pip-services-commons-go/reflect"
+)
+
+type AtLeastOneExistsRule struct {
+	properties []string
+}
+
+func NewAtLeastOneExistsRule(properties ...string) *AtLeastOneExistsRule {
+	return &AtLeastOneExistsRule{
+		properties: properties,
+	}
+}
+
+func (c *AtLeastOneExistsRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	name := path
+	if name == "" {
+		name = "value"
+	}
+	found := 0
+
+	for _, property := range c.properties {
+		propertyValue := reflect.ObjectReader.GetProperty(value, property)
+		if propertyValue != nil {
+			found++
+		}
+	}
+
+	if found == 0 {
+		return []*ValidationResult{
+			NewValidationResult(
+				path,
+				Error,
+				"VALUE_NULL",
+				name+" must have at least one property from "+strings.Join(c.properties, ","),
+				c.properties,
+				nil,
+			),
+		}
+	}
+
+	return nil
+}
