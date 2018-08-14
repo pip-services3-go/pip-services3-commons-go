5877a6494ba162748673c4049fa719c1e3ebef12
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-14 00:00:13 -0700

    Ported NotRule

--- validate/NotRule.go
+++ validate/NotRule.go
@@ -1,3 +1,39 @@
 package validate
 
-// Todo: Complete implementation
+type NotRule struct {
+	rule IValidationRule
+}
+
+func NewNotRule(rule IValidationRule) *NotRule {
+	return &NotRule{
+		rule: rule,
+	}
+}
+
+func (c *NotRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	if c.rule == nil {
+		return nil
+	}
+
+	name := path
+	if name == "" {
+		name = "value"
+	}
+
+	results := c.rule.Validate(path, schema, value)
+
+	if results != nil && len(results) > 0 {
+		return nil
+	}
+
+	return []*ValidationResult{
+		NewValidationResult(
+			path,
+			Error,
+			"NOT_FAILED",
+			"Negative check for "+name+" failed",
+			nil,
+			nil,
+		),
+	}
+}
