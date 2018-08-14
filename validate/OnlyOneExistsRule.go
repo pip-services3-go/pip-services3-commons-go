15f1078e8f2a7d7e7bc565dbb581d93e74ec34af
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-13 23:47:52 -0700

    Ported OnlyOneExistsRule

--- validate/OnlyOneExistsRule.go
+++ validate/OnlyOneExistsRule.go
@@ -1,3 +1,59 @@
 package validate
 
-// Todo: Complete implementation
+import (
+	"strings"
+
+	"github.com/pip-services-go/pip-services-commons-go/reflect"
+)
+
+type OnlyOneExistsRule struct {
+	properties []string
+}
+
+func NewOnlyOneExistsRule(properties ...string) *OnlyOneExistsRule {
+	return &OnlyOneExistsRule{
+		properties: properties,
+	}
+}
+
+func (c *OnlyOneExistsRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	name := path
+	if name == "" {
+		name = "value"
+	}
+
+	found := 0
+
+	for _, property := range c.properties {
+		propertyValue := reflect.ObjectReader.GetProperty(value, property)
+		if propertyValue != nil {
+			found++
+		}
+	}
+
+	if found == 0 {
+		return []*ValidationResult{
+			NewValidationResult(
+				path,
+				Error,
+				"VALUE_NULL",
+				name+" must have at least one property from "+strings.Join(c.properties, ","),
+				c.properties,
+				nil,
+			),
+		}
+	} else if found > 1 {
+		return []*ValidationResult{
+			NewValidationResult(
+				path,
+				Error,
+				"VALUE_ONLY_ONE",
+				name+" must have only one property from "+strings.Join(c.properties, ","),
+				c.properties,
+				nil,
+			),
+		}
+	}
+
+	return nil
+}
