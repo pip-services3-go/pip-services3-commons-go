6e6b6446a8e92f98ebfde2832c945d4cdbe4fe6e
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-14 00:28:30 -0700

    Ported ExcludedRule

--- validate/ExcludedRule.go
+++ validate/ExcludedRule.go
@@ -1,3 +1,60 @@
 package validate
 
-// Todo: Complete implementation
+import (
+	"strings"
+
+	"github.com/pip-services-go/pip-services-commons-go/convert"
+)
+
+type ExcludedRule struct {
+	values []interface{}
+}
+
+func NewExcludedRule(values ...interface{}) *ExcludedRule {
+	return &ExcludedRule{
+		values: values,
+	}
+}
+
+func (c *ExcludedRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	if c.values == nil || len(c.values) == 0 {
+		return nil
+	}
+
+	name := path
+	if name == "" {
+		name = "value"
+	}
+
+	found := false
+
+	for _, thisValue := range c.values {
+		if ObjectComparator.AreEqual(value, thisValue) {
+			found = true
+			break
+		}
+	}
+
+	if found {
+		expectedValues := strings.Builder{}
+		for _, thisValue := range c.values {
+			if expectedValues.Len() > 0 {
+				expectedValues.WriteString(",")
+			}
+			expectedValues.WriteString(convert.StringConverter.ToString(thisValue))
+		}
+
+		return []*ValidationResult{
+			NewValidationResult(
+				path,
+				Error,
+				"VALUE_INCLUDED",
+				name+" must not be one of "+expectedValues.String(),
+				c.values,
+				nil,
+			),
+		}
+	}
+
+	return nil
+}
--- validate/ISchema.go
+++ validate/ISchema.go
@@ -1,4 +1,9 @@
 package validate
 
+import "github.com/pip-services-go/pip-services-commons-go/errors"
+
 type ISchema interface {
+	Validate(value interface{}) []*ValidationResult
+	ValidateAndReturnError(correlationId string, value interface{}, strict bool) *errors.ApplicationError
+	ValidateAndThrowError(correlationId string, value interface{}, strict bool)
 }
