909f5a9d708ef8bafa38a0107dd802ccebdc26f4
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-14 00:25:26 -0700

    Ported IncludedRule

--- validate/IncludedRule.go
+++ validate/IncludedRule.go
@@ -1,3 +1,60 @@
 package validate
 
-// Todo: Complete implementation
+import (
+	"strings"
+
+	"github.com/pip-services-go/pip-services-commons-go/convert"
+)
+
+type IncludedRule struct {
+	values []interface{}
+}
+
+func NewIncludedRule(values ...interface{}) *IncludedRule {
+	return &IncludedRule{
+		values: values,
+	}
+}
+
+func (c *IncludedRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	if c.values == nil || len(c.values) == 0 {
+		return nil
+	}
+
+	name := path
+	if name == "" {
+		name = "value"
+	}
+
+	found := false
+
+	for _, thisValue := range c.values {
+		if ObjectComparator.AreEqual(value, thisValue) {
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		expectedValues := strings.Builder{}
+		for _, thisValue := range c.values {
+			if expectedValues.Len() > 0 {
+				expectedValues.WriteString(",")
+			}
+			expectedValues.WriteString(convert.StringConverter.ToString(thisValue))
+		}
+
+		return []*ValidationResult{
+			NewValidationResult(
+				path,
+				Error,
+				"VALUE_NOT_INCLUDED",
+				name+" must be one of "+expectedValues.String(),
+				c.values,
+				nil,
+			),
+		}
+	}
+
+	return nil
+}
--- validate/ValueComparisonRule.go
+++ validate/ValueComparisonRule.go
@@ -1,3 +1,40 @@
 package validate
 
-// Todo: Complete implementation
+import "github.com/pip-services-go/pip-services-commons-go/convert"
+
+type ValueComparisonRule struct {
+	value     interface{}
+	operation string
+}
+
+func NewValueComparisonRule(operation string, value interface{}) *ValueComparisonRule {
+	return &ValueComparisonRule{
+		value:     value,
+		operation: operation,
+	}
+}
+
+func (c *ValueComparisonRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	name := path
+	if name == "" {
+		name = "value"
+	}
+
+	if !ObjectComparator.Compare(value, c.operation, c.value) {
+		expectedValue := convert.StringConverter.ToString(c.value)
+		actualValue := convert.StringConverter.ToString(value)
+
+		return []*ValidationResult{
+			NewValidationResult(
+				path,
+				Error,
+				"BAD_VALUE",
+				name+" must "+c.operation+" "+expectedValue+" but found "+actualValue,
+				c.operation+" "+expectedValue,
+				value,
+			),
+		}
+	}
+
+	return nil
+}
