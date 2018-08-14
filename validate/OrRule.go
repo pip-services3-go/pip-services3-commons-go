4a15d44f0d03c1d17485f2da2f414108c5c90ed2
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-08-13 23:55:15 -0700

    Ported OrRule

--- validate/OrRule.go
+++ validate/OrRule.go
@@ -1,3 +1,31 @@
 package validate
 
-// Todo: Complete implementation
+type OrRule struct {
+	rules []IValidationRule
+}
+
+func NewOrRule(rules ...IValidationRule) *OrRule {
+	return &OrRule{
+		rules: rules,
+	}
+}
+
+func (c *OrRule) Validate(path string, schema ISchema, value interface{}) []*ValidationResult {
+	if c.rules == nil || len(c.rules) == 0 {
+		return nil
+	}
+
+	results := []*ValidationResult{}
+
+	for _, rule := range c.rules {
+		ruleResults := rule.Validate(path, schema, value)
+
+		if ruleResults == nil || len(ruleResults) == 0 {
+			return nil
+		}
+
+		results = append(results, ruleResults...)
+	}
+
+	return results
+}
