287797de3fa7e76851d579d212c7f3e77dec30a9
Levichev Dmitry <levichev.dmitry@gmail.com>
2019-11-18 15:00:46 +0300

    Add docs for Command

--- commands/Event.go
+++ commands/Event.go
@@ -4,10 +4,32 @@ import (
 	"github.com/pip-services3-go/pip-services3-commons-go/run"
 )
 
+/*
+Concrete implementation of IEvent interface. It allows to send asynchronous
+notifications to multiple subscribed listeners.
+Example:
+event: = NewEvent("my_event");
+
+event.AddListener(myListener);
+
+event.Notify("123", Parameters.fromTuples(
+  "param1", "ABC",
+  "param2", 123
+));
+*/
+
 type Event struct {
 	name      string
 	listeners []IEventListener
 }
+
+// Creates a new event and assigns its name.
+// throws
+// an Error if the name is null.
+// Parameters:
+// 				- name: string
+// 					the name of the event that is to be created.
+// Returns Event
 
 func NewEvent(name string) *Event {
 	if name == "" {
@@ -20,17 +42,35 @@ func NewEvent(name string) *Event {
 	}
 }
 
+// Gets the name of the event.
+// Returns string
+// the name of this event.
+
 func (c *Event) Name() string {
 	return c.name
 }
+
+// Gets all listeners registred in this event.
+// Returns []IEventListener
+// a list of listeners.
 
 func (c *Event) Listeners() []IEventListener {
 	return c.listeners
 }
 
+// Adds a listener to receive notifications when this event is fired.
+// Parameters:
+// 				- listener: IEventListener
+// 					the listener reference to add.
+
 func (c *Event) AddListener(listener IEventListener) {
 	c.listeners = append(c.listeners, listener)
 }
+
+// Removes a listener, so that it no longer receives notifications for this event.
+// Parameters:
+//  			- listener: IEventListener
+// 				the listener reference to remove.
 
 func (c *Event) RemoveListener(listener IEventListener) {
 	for i, l := range c.listeners {
@@ -40,6 +80,13 @@ func (c *Event) RemoveListener(listener IEventListener) {
 		}
 	}
 }
+
+// Fires this event and notifies all registred listeners.
+// Parameters:
+// 				- correlationId: string
+// 				(optional) transaction id to trace execution through call chain.
+// 				- args: Parameters
+// 				the parameters to raise this event with.
 
 func (c *Event) Notify(correlationId string, args *run.Parameters) {
 	for _, listener := range c.listeners {
--- commands/ICommandable.go
+++ commands/ICommandable.go
@@ -1,5 +1,27 @@
 package commands
 
+/*
+An interface for commandable objects, which are part of the command design pattern.
+The commandable object exposes its functonality as commands and events groupped into a CommandSet.
+
+This interface is typically implemented by controllers and is used to auto generate external interfaces.
+
+type MyDataController {
+  _commandSet  CommandSet;
+}
+  func (dc *MyDataController) getCommandSet() CommandSet {
+    if (dc._commandSet == nil)
+      dc._commandSet = NewDataCommandSet();
+    return dc._commandSet;
+  }
+
+*/
+
 type ICommandable interface {
+	// 	Gets a command set with all supported commands and events.
+	// see
+	// CommandSet
+	// Returns *CommandSet
+	// a command set with commands and events.
 	GetCommandSet() *CommandSet
 }
