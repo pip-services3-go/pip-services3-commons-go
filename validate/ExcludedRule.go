c71af495517f7867bbe3d04736e2707a72b1c97e
Sergey Seroukhov <seroukhov@gmail.com>
2020-01-20 11:49:13 -0700

    Fixed mistyping

--- validate/ExcludedRule.go
+++ validate/ExcludedRule.go
@@ -3,7 +3,7 @@ package validate
 import (
 	"strings"
 
-	"github.com/pip-services3-go/pip-services3-commons-go/v3/convet"
+	"github.com/pip-services3-go/pip-services3-commons-go/v3/convert"
 )
 
 /*
