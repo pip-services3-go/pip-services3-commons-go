8f3fa214e2294b67b70f55a09191cd33f982ab81
Dmitriy Krayniy <krdima92@gmail.com>
2020-12-07 10:55:59 +0300

    added github actions pipeline

--- clean.ps1
+++ clean.ps1
@@ -1,19 +1,24 @@
 #!/usr/bin/env pwsh
 
-# Get component data and set necessary variables
+# Recreate image names using the data in the "component.json" file
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
 $buildImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-build"
+$docsImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
 $testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
-$rcImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-rc"
 
 # Clean up build directories
-Get-ChildItem -Path "." -Include "obj" -Recurse | foreach($_) { Remove-Item -Force -Recurse $_.FullName }
+Get-ChildItem -Path "." -Include "exe" -Recurse | foreach($_) { Remove-Item -Force -Recurse $_.FullName }
 
 # Remove docker images
 docker rmi $buildImage --force
+docker rmi $docsImage --force
 docker rmi $testImage --force
-docker rmi $rcImage --force
 docker image prune --force
+docker rmi -f $(docker images -f "dangling=true" -q) # remove build container if build fails
 
 # Remove existed containers
-docker ps -a | Select-String -Pattern "Exit" | foreach($_) { docker rm $_.ToString().Split(" ")[0] }
+$exitedContainers = docker ps -a | Select-String -Pattern "Exit"
+foreach($c in $exitedContainers) { docker rm $c.ToString().Split(" ")[0] }
+
+# Remove unused volumes
+docker volume rm -f $(docker volume ls -f "dangling=true")
--- test.ps1
+++ test.ps1
@@ -3,7 +3,6 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Get component data and set necessary variables
 $component = Get-Content -Path "component.json" | ConvertFrom-Json
 $testImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-test"
 
@@ -15,7 +14,14 @@ try {
     docker-compose -f ./docker/docker-compose.test.yml down
 
     docker-compose -f ./docker/docker-compose.test.yml up --build --abort-on-container-exit --exit-code-from test
+
+    # Save the result to avoid overwriting it with the "down" command below
+    $exitCode = $LastExitCode 
 } finally {
     # Workaround to remove dangling images
     docker-compose -f ./docker/docker-compose.test.yml down
 }
+
+# Return the exit code of the "docker-compose.test.yml up" command
+exit $exitCode 
+
