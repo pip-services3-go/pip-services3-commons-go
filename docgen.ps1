35b956d7cfde98cd38ae00418e020fd530c782ba
Dmitry Krayniy <krdima92@gmail.com>
2022-08-11 08:50:54 +0400

    Updated delivery pipeline

--- docgen.ps1
+++ docgen.ps1
@@ -3,38 +3,37 @@
 Set-StrictMode -Version latest
 $ErrorActionPreference = "Stop"
 
-# Generate image and container names using the data in the "component.json" file
-$component = Get-Content -Path "component.json" | ConvertFrom-Json
+# Generate image and container names using the data in the "$PSScriptRoot/component.json" file
+$component = Get-Content -Path "$PSScriptRoot/component.json" | ConvertFrom-Json
 
-$docImage="$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
-$container=$component.name
+$docImage = "$($component.registry)/$($component.name):$($component.version)-$($component.build)-docs"
+$container = $component.name
 
 # Remove build files
-if (Test-Path "./docs") {
-    Remove-Item -Recurse -Force -Path "./docs/*"
+if (Test-Path "$PSScriptRoot/docs") {
+    Remove-Item -Recurse -Force -Path "$PSScriptRoot/docs/*"
 } else {
-    $null = New-Item -ItemType Directory -Force -Path "./docs"
+    New-Item -ItemType Directory -Force -Path "$PSScriptRoot/docs"
+}
+
+# Copy private keys to access git repo
+if (-not (Test-Path -Path "$PSScriptRoot/docker/id_rsa")) {
+    if ($env:GIT_PRIVATE_KEY -ne $null) {
+        Set-Content -Path "$PSScriptRoot/docker/id_rsa" -Value $env:GIT_PRIVATE_KEY
+    } else {
+        Copy-Item -Path "~/.ssh/id_rsa" -Destination "$PSScriptRoot/docker"
+    }
 }
 
 # Build docker image
-docker build -f docker/Dockerfile.docgen -t $docImage .
+docker build -f "$PSScriptRoot/docker/Dockerfile.docs" -t $docImage .
 
-# Run docgen container
-docker run -d --name $container $docImage
-# Wait it to start
-Start-Sleep -Seconds 2
-# Generate docs
-docker exec -ti $container /bin/bash -c "wget -r -np -N -E -p -k http://localhost:6060/pkg/"
-# Copy docs from container
-docker cp "$($container):/app/localhost:6060/pkg" ./docs/pkg
-docker cp "$($container):/app/localhost:6060/lib" ./docs/lib
-# Remove docgen container
-docker rm $container --force
+# Create and copy compiled files, then destroy the container
+docker create --name $container $docImage
+docker cp "$($container):/go/eic/docs" "$PSScriptRoot/"
+docker rm $container
 
-Write-Output "<head><meta http-equiv='refresh' content='0; URL=./pkg/index.html'></head>" > ./docs/index.html
-
-# Verify docs 
-if (!(Test-Path "./docs")) {
-    Write-Host "docs folder doesn't exist in root dir. Watch logs above."
-    exit 1
+# Verify that docs folder was indeed created after generating documentation
+if (-not (Test-Path "$PSScriptRoot/docs")) {
+    Write-Error "docs folder doesn't exist in root dir. Build failed. See logs above for more information."
 }
